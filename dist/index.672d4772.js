466ba376d553dce912a3bc91c739d7b03f1dadc6
Himanshu Rai <swavyast@gmail.com>
2024-03-15 09:00:29 +0530

    initial commit as a submodule of reactProjects

---
+++ dist/index.672d4772.js
@@ -0,0 +1,3 @@
+//alert('Script is loading')
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ script.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client"
+//alert('Script is loading')
+const h1 = <h1>Hello World</h1>
+
+const root =ReactDOM.createRoot(document.querySelector('#root'))
+root.render(h1)
